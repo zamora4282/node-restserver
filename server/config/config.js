5d849a61b4ab82390e3ea718fe9ba844edf23cf3
David <zamora4282@gmail.com>
2020-05-11 11:36:07 +0200

    Variable de entorno MONGO

--- server/config/config.js
+++ server/config/config.js
@@ -18,7 +18,7 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb+srv://zamora4282:toYO6S6WwoFV8UIm@cluster0-z9thc.mongodb.net/cafe';
+    urlDB = process.env.MONGO_URI;
 }
 
 process.env.URLDB = urlDB;
