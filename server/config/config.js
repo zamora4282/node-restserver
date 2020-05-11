b5cc7394720240cef4d7fb8d734cbb21d59bf31d
David <zamora4282@gmail.com>
2020-05-11 16:12:33 +0200

    Login normal seccion 10

--- server/config/config.js
+++ server/config/config.js
@@ -22,3 +22,16 @@ if (process.env.NODE_ENV === 'dev') {
 }
 
 process.env.URLDB = urlDB;
+
+// ===========================
+// Vencimiento del Token
+// ===========================
+// 60 segundos
+// 60 minutos
+// 24 horas
+process.env.CADUCIDAD_TOKEN = 60 * 60 * 24 * 30;
+
+// ===========================
+// SEED de autentificacion
+// ===========================
+process.env.SEED = process.env.SEED || 'este-es-el-seed-desarrollo'
